b60628590dbbce8aba8f9b10db9d262517c266a2
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 20:48:49 +0100

    fix: Update pre-release.sh

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -4,8 +4,8 @@ set -e
 sed -i -E "s/(Version\(string: \")[^\"]*(\"\))/\1$NEW_TAG\2/" VERSION.swift
 
 # Configure git
-git config user.name github-actions
-git config user.email github-actions@github.com
+git config user.name "Shape Games CI"
+git config user.email "ci@shapegames.com"
 
 # Commit and push changes
 git add VERSION.swift
