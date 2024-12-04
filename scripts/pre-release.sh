0f320c37597ae1ae6d14c69fe5a63b3358aae89c
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 18:40:36 +0100

    fix: Update pre-release.sh

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-# Example: Update a specific file with the new tag
-echo "$NEW_TAG" > VERSION.txt
+sed -i '' "s/\"[^\"]*\"/\"$NEW_VERSION\"/g" VERSION.swift
 
 # Configure git
 git config user.name github-actions
 git config user.email github-actions@github.com
 
 # Commit and push changes
-git add VERSION.txt
+git add VERSION.swift
 git commit -m "chore: update version to $NEW_TAG"
 git push
