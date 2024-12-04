ac3981a3f8d1827f4b87d81ee9d4ebd4f6ce1bd1
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 20:24:32 +0100

    fix: Update pre-commit.js

--- pre-commit.js
+++ pre-commit.js
@@ -1,8 +1,18 @@
-interface Props {
-  tag: string;
-  version: string; // Next version e.g. 1.12.3
-}
+const fs = require('fs');
+const { execSync } = require('child_process');
 
-export function preCommit(props: Props): void {
-  console.log(props);
+exports.preCommit = (props) => {
+  assert.ok(props.tag, 'tag should not be empty')
+
+  const filePath = 'VERSION.swift';
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const updatedContent = fileContent.replace(/(Version\(string: \")[^\"]*(\"\))/g, `$1${props.tag}$2`);
+  fs.writeFileSync(filePath, updatedContent, 'utf8');
+
+  execSync('git config user.name github-actions');
+  execSync('git config user.email github-actions@github.com');
+
+  execSync('git add VERSION.swift');
+  execSync(`git commit -m "chore: update version to ${props.tag}"`);
+  execSync('git push');
 }
