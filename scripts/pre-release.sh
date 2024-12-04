061be1f629514f74e201b5f8aacb18c3207ea496
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 20:50:07 +0100

    fix: Update pre-release.sh

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -10,5 +10,5 @@ git config user.email "ci@shapegames.com"
 # Commit and push changes
 git add VERSION.swift
 git commit -m "Update SDK version to $NEW_TAG"
-git tag -a $NEW_TAG -m "$NEW_TAG" -f
+git tag $NEW_TAG -f
 git push origin HEAD $NEW_TAG --force
