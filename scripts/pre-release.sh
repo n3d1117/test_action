629ba8a562db200db6a98a3fdcb2a1791a7d10b4
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 20:37:29 +0100

    fix: Update pre-release.sh

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -3,9 +3,12 @@ set -e
 
 sed -i -E "s/(Version\(string: \")[^\"]*(\"\))/\1$NEW_TAG\2/" VERSION.swift
 
+# Configure git
+git config user.name github-actions
+git config user.email github-actions@github.com
+
 # Commit and push changes
 git add VERSION.swift
-git commit -m "chore: update version to $NEW_TAG"
+git commit -m "Update SDK version to $NEW_TAG"
 git tag -a $NEW_TAG -m "$NEW_TAG" -f
-git push origin main --follow-tags
-git push origin $NEW_TAG -f
+git push origin HEAD $NEW_TAG --force
