b0f050852133d88a32494d0dc08d1731d3dcbab4
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 18:35:10 +0100

    Update and rename VERSION.txt to VERSION.swift

---
+++ VERSION.swift
@@ -0,0 +1,12 @@
+//
+//  GamesSDK+Version.swift
+//  ShapeGamesSDK
+//
+//  Created by ned on 03/12/24.
+//
+
+import GamesFoundation
+
+extension SDK {
+    public static let version = Version(string: "1.0.0")!
+}
