4f4264693c224bba331cbe8e6c0f431e94755d19
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 20:25:48 +0100

    fix: Update pre-commit.js

--- pre-commit.js
+++ pre-commit.js
@@ -1,18 +1,19 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+const fs = require('fs')
+const assert = require('assert')
 
 exports.preCommit = (props) => {
+  const {GITHUB_WORKSPACE} = process.env;
+
+  assert.ok(GITHUB_WORKSPACE, 'GITHUB_WORKSPACE should not be empty')
   assert.ok(props.tag, 'tag should not be empty')
+  assert.ok(props.version, 'version should not be empty')
 
-  const filePath = 'VERSION.swift';
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const updatedContent = fileContent.replace(/(Version\(string: \")[^\"]*(\"\))/g, `$1${props.tag}$2`);
-  fs.writeFileSync(filePath, updatedContent, 'utf8');
+  const body = {
+    workspace: GITHUB_WORKSPACE,
+    tag: props.tag,
+    version: props.version,
+    random: Math.random(),
+  }
 
-  execSync('git config user.name github-actions');
-  execSync('git config user.email github-actions@github.com');
-
-  execSync('git add VERSION.swift');
-  execSync(`git commit -m "chore: update version to ${props.tag}"`);
-  execSync('git push');
+  fs.writeFileSync('pre-commit.test.json', JSON.stringify(body, null, 2))
 }
