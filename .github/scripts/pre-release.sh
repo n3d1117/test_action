431cfbf8a1fdb4837a6ae2a1da214ee7b6ae84ea
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-04 18:39:16 +0100

    fix: Update pre-release.sh

--- .github/scripts/pre-release.sh
+++ .github/scripts/pre-release.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-sed -i '' "s/\"[^\"]*\"/\"$NEW_VERSION\"/g" Version.swift
+sed -i '' "s/\"[^\"]*\"/\"$NEW_VERSION\"/g" VERSION.swift
 
 # Configure git
 git config user.name github-actions
 git config user.email github-actions@github.com
 
 # Commit and push changes
-git add VERSION.txt
+git add VERSION.swift
 git commit -m "chore: update version to $NEW_TAG"
 git push
