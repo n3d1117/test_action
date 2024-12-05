f4520d6f5cfe560064e88b0ebf44f7aef68f0c7a
Edoardo <11541888+n3d1117@users.noreply.github.com>
2024-12-05 17:11:39 +0100

    fix: Update VERSION.swift

--- VERSION.swift
+++ VERSION.swift
@@ -1,11 +1,6 @@
 //
-//  GamesSDK+Version.swift
-//  ShapeGamesSDK
+//  Version.swift
 //
-//  Created by ned on 03/12/24.
-//
-
-import GamesFoundation
 
 extension SDK {
     public static let version = Version(string: "1.0.35")!
